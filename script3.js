e6a2048ad2c02bd90fa635efd84a0df86640990b
Novitatri31 <triwahyuningsih931@gmail.com>
2022-10-03 14:21:39 +0700

    Latihan looping bersarang skysen solution

---
+++ script3.js
@@ -0,0 +1,58 @@
+// SOAL 1
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15;
+
+// const counter = 5;
+// let value = 1;
+
+// for (let i = 0; i < counter; i++) {
+
+//     let s = ""
+//     for (let j = 0; j <= i; j++){
+//         s += value++ + " ";
+//     }
+//     console.log (s);
+//   }  
+// 1 2 3 4 5
+// 6 7 8 9 10 
+// 11 12 13 14 15
+
+// const counter = 5;
+// let value = 1;
+
+// for (let i = 0; i < counter; i++) {
+
+//     let s = ""
+//     for (let j = 0; j < counter - i; j++){
+//         s += (i + j + 1) + " ";
+//     }
+//     console.log (s);
+//   }  
+
+// 1 2 3 4 5
+// 2 3 4 5
+// 3 4 5
+// 4 5
+// 5
+// const counter = 5;
+
+// for (let i = 0; i < counter; i++) {
+    
+//     let value = i + 1;
+//     let s = value + " ";
+//     for (let j = 1; j <= i; j++){
+//         value += counter - j; 
+//         s += value + " ";
+//     }
+//     console.log (s);
+//   }  
+
+// 1 6 11
+// 2 7 12
+// 3 8 13
+// 4 9 14
+// 5 10 15
+
